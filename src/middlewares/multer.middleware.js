793a1f232ce5ae9fa919f74dc2b0b707667915db
venom <baibhav.229302366@muj.manipal.edu>
2025-08-25 17:03:16 +0530

    upload pic and videos

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer"  // Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files. It is written on top of busboy for maximum efficiency.
+
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+export const upload = multer({ storage,
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'
+
+ cloudinary.config({  
+        cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME',      //CLODINARY CONfig to setup api keys
+        api_key: 'process.env.CLOUDINARY_API_KEY', 
+        api_secret: 'process.env.CLOUDINARY_API_SECRET' 
+    });
+
+
+    
+const uploadToCloudinary = async(localFilepath) => {
+    try {
+        if(!localFilepath) return null   //upload the file on the clodinary
+       const response = await cloudinary.uploader.upload(localFilepath, {
+            resource_type:"auto"
+        })   // file is uploaded successfully
+
+        console.log("File uploaded successfully:", response.url);
+        return response.url;
+    } catch (error) {
+        fs.unlinkSync(localFilepath)  // delete the file from local server
+        return null;
+
+    }
+}
+
+    
