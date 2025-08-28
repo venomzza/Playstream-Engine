08e2575ee6e2ad1cf76122d396f5a33b96e0a93a
venom <baibhav.229302366@muj.manipal.edu>
2025-08-28 18:36:25 +0530

    minor bug solved

--- src/app.js
+++ src/app.js
@@ -14,5 +14,16 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))  // used to parse
 app.use(express.static("public"))  // used to serve static files from the public directory
 app.use(cookieParser())  // used to parse cookies from the request
 
+//routes
+
+import userRouter from './routes/user.routes.js'
+// app.use('/api/users', userRouter)
+
+
+//route declarations
+
+/* app.use("/users", userRouter)   // yaha /user ek command h jo req aa rhi h user side se ya gateway h jb ye aaye toh response me userRoute pe routing karni h.......    */
+ 
+app.use("/api/v1/users", userRouter)
 
 export {app}
